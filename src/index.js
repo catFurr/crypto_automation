cafa046ce0936ab764a9a6616c5e15c15deaaad9
ibrahim ali <iamcatfurr@gmail.com>
2024-03-15 15:42:50 +0300

    added code, cron trigger

--- src/index.js
+++ src/index.js
@@ -10,9 +10,46 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+		let resp = await handleEvent(env)
+
+		return new Response(resp);
+	},
+
+	async scheduled(event, env, ctx) {
+		ctx.waitUntil(handleEvent(env));
 	},
 };
+
+async function handleEvent (env) {
+	const COINMARKETCAP_API = env.COINMARKETCAP_API_KEY
+	const COINMARKETCAP_URL = env.COINMARKETCAP_URL
+	const DISCORD_URL = env.DISCORD_URL
+	try {
+		let { btcPrice } = await getLatestPrice(COINMARKETCAP_API, COINMARKETCAP_URL)
+
+		const threshold = 70000;
+		let isThresholdReached = compareThreshold(btcPrice, threshold)
+
+		if (isThresholdReached) {
+			let message = "Bitcoin Price Fell Below the Threshold!";
+			message += "\ncurrent price: " + String(btcPrice)
+			message += "\nthreshold: " + String(threshold)
+			await callDiscordHook(DISCORD_URL, message)
+		}
+	} catch (error) {
+		let message = "Error in fetch handler: " + String(error);
+		console.error(message)
+		try {
+			await callDiscordHook(DISCORD_URL, message)
+		} catch (discord_error) {
+			console.error("Error in calling Discord: ", discord_error)
+		}
+
+		return message;
+	}
+
+	return "Completed.";
+}
 
 // Fetch the latest coin prices
 // Either throw an error or
@@ -21,12 +58,54 @@ export default {
 // 	btcPrice: Number(),
 // 	ethPrice: Number()
 // }
-export async function getLatestPrice (api_key, url) {}
+export async function getLatestPrice (api_key, url) {
+	const params = {
+		symbol: 'BTC,ETH',
+		convert: 'USD'
+	};
+
+	var response = await fetch(`${url}?${new URLSearchParams(params)}`, {
+		headers: {
+		'X-CMC_PRO_API_KEY': api_key
+		}
+	});
+
+	if (!response.ok) {
+		throw new Error("Server returned bad response code: " + String(response.status))
+	}
+
+	const data = await response.json();
+
+	const btcData = data.data.BTC;
+	const ethData = data.data.ETH;
+
+	return {
+		btcPrice: Math.round(btcData.quote.USD.price),
+		ethPrice: Math.round(ethData.quote.USD.price)
+	};
+}
 
 // Compare the coin price with a threshold
-// Either throw an error or return bool
-export function compareThreshold (newPrice, threshold) {}
+// return true if newPrice is lower, else false
+export function compareThreshold (newPrice, threshold) {
+	return Number(newPrice) < Number(threshold)
+}
 
 // Calls the Discord Webhook URL
 // Either throw an error or return void
-export async function callDiscordHook (url, message) {}
+export async function callDiscordHook (url, message) {
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+	const urlencoded = new URLSearchParams();
+	urlencoded.append("content", message);
+
+	var response = await fetch(url, {
+		method: "POST",
+		headers: myHeaders,
+		body: urlencoded
+	});
+
+	if (!response.ok) {
+		throw new Error("Server returned bad response code: " + String(response.status))
+	}
+}
